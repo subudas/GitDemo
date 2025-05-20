eb915c15ae6d3113000906e8ce4b066a266984fb
subu <subu.electronics26@gmail.com>
2025-05-20 15:23:56 +0530

    American guy commit

--- Print.py
+++ Print.py
@@ -2,10 +2,10 @@ print("Hello")
 print("Hi")
 
 def calculator(a,b):
-    print (f"The addition is:{a+b}")
+    print (f"The addition is:{a+b}") #This is for addition
     print (f"The subtraction is:{a-b}")
     print (f"The multiplication is:{a*b}")
-    print (f"The division is: {a/b}")
+    print (f"The division is: {a/b}")    
 
 a = calculator(5,3)
 
